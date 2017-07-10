093d915cc001bd4ab7f71f8b3b728e2a7e812771
Mark Lind <mglind@deseretdigital.com>
2017-07-10 17:59:21 -0600

    Adds a new endpoint in order to get recommendation data from the API

---
+++ src/Widget/PublicData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CxInsightSDK\Widget;
+
+use CxInsightSDK\BaseSDK;
+
+class PublicData extends BaseSDK
+{
+    /**
+     * @{inheritDoc}
+     */
+    protected $requestPath = '/public/widget/data';
+}
