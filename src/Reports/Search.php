8eb6ed1d57ef66feb6bd0b18eb4fc2c62ecb0b0a
Russell Ahlstrom <rahlstrom@deseretdigital.com>
2017-09-07 21:18:21 -0600

    Adds Endpoints for /reports/search and /reports/search/usage
    Adds classes to work with the /reports/search and the
    /reports/search/usage endpoints.

---
+++ src/Reports/Search.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CxInsightSDK\Reports;
+
+use CxInsightSDK\BaseSDK;
+
+class Search extends BaseSDK
+{
+    /**
+     * @{inheritDoc}
+     */
+    protected $requestPath = '/reports/search';
+}
